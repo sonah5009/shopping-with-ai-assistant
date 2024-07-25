d85eab8acebac02a96f43a0b0e3192010e8dfdf0
sonah5009 <sonah5009@gmail.com>
2024-07-25 17:17:44 +0200

    add: csv data processing

---
+++ dataprocessing.py
@@ -0,0 +1,20 @@
+import pandas as pd
+df = pd.read_csv('e-commerce-data-origin.csv', encoding='ISO-8859-1', engine='python')
+
+# Description(Name of the product)
+# Quantity
+# UnitPrice
+
+selected_columns = ['Description', 'Quantity', 'UnitPrice']
+df = df[selected_columns]
+print(pd.isna(df).sum(), '\n')
+
+df = df.dropna()
+df.drop_duplicates(inplace=True)
+
+# Rename Description to Name
+df.rename(columns={'Description':'Name'}, inplace=True)
+
+
+# Save the csv file with only Description, Quantity, UnitPrice row
+df.to_csv("e-commerce-data.csv", sep=',', index=False)
